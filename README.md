400a3f60891865a163796cf6b2fb3aeb2d9e1a7f
Artem <64469607+mysticpillow@users.noreply.github.com>
2023-12-05 22:58:43 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AvaloniaTemplate
+Starter template for avalonia development.\
+Uses .Net 8.0 and MVVM.CommunityToolkit.
---
+++ TemplateApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TemplateApp.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
