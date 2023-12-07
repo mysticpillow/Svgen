caf5c6821d95a343a504b4beecb47c6d8aa700b1
mysticpillow <batt098@gmail.com>
2023-12-08 01:53:32 +0500

    Add `Avalonia.Svg.Skia` nuget package

--- src/SvgenDemo/Program.cs
+++ src/SvgenDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia;
+using Avalonia.Svg.Skia;
 using System;
 
 namespace TemplateApp;
@@ -14,8 +15,12 @@ sealed class Program
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        GC.KeepAlive(typeof(SvgImageExtension).Assembly);
+        GC.KeepAlive(typeof(Avalonia.Svg.Skia.Svg).Assembly);
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
+    }
 }
