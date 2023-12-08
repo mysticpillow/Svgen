7c495eeea6bdd2ecd773198b448a1ce617734447
mysticpillow <batt098@gmail.com>
2023-12-09 00:51:27 +0500

    Use lazy initialization and `SvgImage` objects

--- SvgenDemo.Resources/SvgResouces.cs
+++ SvgenDemo.Resources/SvgResouces.cs
@@ -1,11 +1,41 @@
-﻿namespace SvgenDemo.Resources
+﻿using Avalonia.Svg.Skia;
+
+namespace SvgenDemo.Resources
 {
     public static class SvgResources
     {
-        public static string A => "avares://SvgenDemo.Resources/SvgImages/A.svg";
-        public static string B => "avares://SvgenDemo.Resources/SvgImages/B.svg";
-        public static string C => "avares://SvgenDemo.Resources/SvgImages/C.svg";
-        public static string D => "avares://SvgenDemo.Resources/SvgImages/D.svg";
-        public static string E => "avares://SvgenDemo.Resources/SvgImages/E.svg";
+        private static SvgImage? _a;
+        public static SvgImage A
+        {
+            get
+            {
+                _a ??= new() { Source = SvgSource.Load<SvgSource>(APath, new Uri(APath)) };
+                return _a;
+            }
+        }
+
+        private static SvgImage? _b;
+        public static SvgImage B
+        {
+            get
+            {
+                _b ??= new() { Source = SvgSource.Load<SvgSource>(BPath, new Uri(BPath)) };
+                return _b;
+            }
+        }
+
+        private static SvgImage? _c;
+        public static SvgImage C
+        {
+            get
+            {
+                _c ??= new() { Source = SvgSource.Load<SvgSource>(CPath, new Uri(CPath)) };
+                return _c;
+            }
+        }
+
+        public static string APath => "avares://SvgenDemo.Resources/SvgImages/A.svg";
+        public static string BPath => "avares://SvgenDemo.Resources/SvgImages/B.svg";
+        public static string CPath => "avares://SvgenDemo.Resources/SvgImages/C.svg";
     }
 }
